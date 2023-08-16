75857470e6d230499a1cd2401a1be2a265dd7753
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-08-16 15:29:30 +0000

    in

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     for i in range(0, len(matrix)):
         row = []
         for j in range(0, len(matrix[i])):
-                row.append(matrix[i][j] * matrix[i][j])
+            row.append(matrix[i][j] * matrix[i][j])
         new_matrix.append(row)
     return new_matrix
