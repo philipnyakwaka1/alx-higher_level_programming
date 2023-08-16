6399e166b1b2eb9c7c55090ec27d838705bcc9a7
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-08-16 15:07:04 +0000

    2d array

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in range(0, len(matrix)):
+        row = []
+        for j in range(0, len(matrix[i])):
+                row.append(matrix[i][j] * matrix[i][j])
+        new_matrix.append(row)
+    return new_matrix
