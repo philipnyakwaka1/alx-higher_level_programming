3b29e0ac216222b86a10bdcfae86dac659215844
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-08-17 05:34:06 +0000

    9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        best_score = 0
+        student =""
+        for key, value in a_dictionary.items():
+            if value > best_score:
+                best_score = value
+                student = key
+        return student
