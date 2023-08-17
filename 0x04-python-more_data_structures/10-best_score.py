09d87cc9cd673849a06c315d6d8837f5fd3e0e02
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-08-17 05:41:05 +0000

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
+    elif len(a_dictionary) == 1:
+        for key, value in a_dictionary.items():
+            return key
     else:
         best_score = 0
         student = ""
