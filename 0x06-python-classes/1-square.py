8104b410c51b700f3e78ce7256c640d754c6bf49
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-08-30 18:49:24 +0000

    add private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines an class Square"""
+
+
+class Square:
+    """This is a class square"""
+
+    def __init__(self, size):
+        """__init__ method initializes the size of the new square
+
+        Args:
+            size: size of the square
+        """
+        self.__size = size
