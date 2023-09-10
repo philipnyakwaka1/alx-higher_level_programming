c661d95f6cc5e91c3644ec07a5e9cf3cbde88980
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-09-10 23:45:36 +0300

    test for max_integer

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,7 +10,7 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         """This is a test function"""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([1, 2, 3]), 3)
+        self.assertEqual(max_integer([1, 4, 3]), 4)
         self.assertEqual(max_integer([1, 1, 1, 1]), 1)
         self.assertEqual(max_integer([-1, -2, -4]), -1)
         self.assertEqual(max_integer([]), None)
