a1b4f584b43c7dc4b2a19f84a5e737e2faa18b1a
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-09-10 20:08:47 +0300

    created test for say_my_name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+"""This modules defines say_my_name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """This function prints the first name and last name
+    Args:
+        first_name: a person's first name
+        last_name: a users last name
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f'My name is {first_name} {last_name}')
