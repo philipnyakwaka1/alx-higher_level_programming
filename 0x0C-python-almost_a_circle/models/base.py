d9124a763936ff9e5757094113fde3243ec8b64d
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-09-18 18:25:17 +0300

    created Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!usr/bin/python3
+"""This is a module for the Base clase"""
+
+
+class Base():
+    """This is the base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """The __init__ method initializes the class
+        Args:
+            id: id attribute of the class
+        """
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+x = Base()
+print(x.id)
+x = Base(1)
+print(x.id)
+x = Base(-1)
+print(x.id)
