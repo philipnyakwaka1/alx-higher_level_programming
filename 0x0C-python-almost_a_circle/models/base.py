6b7153f7d120ed944d33b8a9c75f3211037989f4
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-09-20 10:54:48 +0300

    added to_json_dictionary method in class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This is a module for the Base clase"""
 
+import json
+
 
 class Base():
     """This is the base class"""
@@ -17,3 +19,11 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """converts a list of dictionaries"""
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
