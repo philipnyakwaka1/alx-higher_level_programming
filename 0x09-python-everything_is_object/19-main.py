ed31b50846001610a38d15d575149bf4e49f1cac
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-09-22 00:17:32 +0300

    103-line2.txt

--- 0x09-python-everything_is_object/19-main.py
+++ 0x09-python-everything_is_object/19-main.py
@@ -1,2 +1,8 @@
-a = 5
-a.x = 1
+def my_function():
+    # Function code here
+
+    my_function.my_attribute = getattr(
+        my_function, "my_attribute", "Hello, I'm an attribute!")
+
+
+print(my_function.my_attribute)  # Output: Hello, I'm an attribute!
