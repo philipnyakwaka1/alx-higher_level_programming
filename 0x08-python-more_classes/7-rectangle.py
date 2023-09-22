ba9c36956b8d2a9407b841bf89f159778cb02780
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-09-22 16:37:58 +0300

    num

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle(object):
     """Defines class Rectangle"""
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """Initializes class Rectangle"""
@@ -65,7 +66,7 @@ class Rectangle(object):
         my_string = ""
         for i in range(self.__height):
             for j in range(self.__width):
-                my_string += "#"
+                my_string += str(Rectangle.print_symbol)
             my_string += "\n"
         return my_string[:-1]
 
