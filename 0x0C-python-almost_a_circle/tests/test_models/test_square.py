28b29ddc438aa5e5970f391196c78ac613daaecf
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-09-22 10:38:37 +0300

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -2,7 +2,7 @@
 
 
 from models.base import Base
-from models.rectangle import Rectangle
+from models.square import Square
 import unittest
 
 
@@ -12,22 +12,20 @@ class TestRectangle(unittest.TestCase):
     def test_positives(self):
         """test for all positive integers"""
 
-        r = Rectangle(5, 10, 1, 2, 3)
-        self.assertEqual(r.width, 5)
-        self.assertEqual(r.height, 10)
-        self.assertEqual(r.x, 1)
-        self.assertEqual(r.y, 2)
-        self.assertEqual(r.id, 3)
+        r = Square(5, 10, 1, 2)
+        self.assertEqual(r.size, 5)
+        self.assertEqual(r.x, 10)
+        self.assertEqual(r.y, 1)
+        self.assertEqual(r.id, 2)
 
     def test_area(self):
         """test for all positive integers"""
 
-        r = Rectangle(5, 10, 1, 2, 3)
-        self.assertEqual(r.area(), 50)
-        r = Rectangle(5, 10, 1, 2, 3)
-        r.width = 2
-        r.height = 4
-        self.assertEqual(8, r.area())
+        r = Square(5, 10, 1, 2)
+        self.assertEqual(r.area(), 25)
+        r = Square(5, 10, 1, 2)
+        r.size = 10
+        self.assertEqual(100, r.area())
         with self.assertRaises(TypeError):
             r.area(1)
 
@@ -35,30 +33,26 @@ class TestRectangle(unittest.TestCase):
         """test for all negative integers"""
 
         with self.assertRaises(ValueError):
-            r = Rectangle(-5, 10, 1, 2, 3)
+            r = Square(-5, 10, 1, 2)
         with self.assertRaises(ValueError):
-            r = Rectangle(5, -10, 1, 2, 3)
+            r = Square(5, -10, 1, 2)
         with self.assertRaises(ValueError):
-            r = Rectangle(5, 10, -1, 2, 3)
-        with self.assertRaises(ValueError):
-            r = Rectangle(5, 10, 1, -2, 3)
+            r = Square(5, 10, -1, 2)
 
     def test_instance(self):
         """Tests if a rectangle instance is instance of Base"""
-        self.assertIsInstance(Rectangle(2, 5), Base)
+        self.assertIsInstance(Square(2, 5), Base)
 
     def test_stringArgs(self):
         """Tests for only two arguments"""
 
         with self.assertRaises(TypeError):
-            r = Rectangle("one", 4)
+            r = Square("one", 4)
         with self.assertRaises(TypeError):
-            r = Rectangle(5, "4")
+            r = Square(5, "4")
         with self.assertRaises(TypeError):
-            r = Rectangle(5, 10, 1, "2", 3)
-        with self.assertRaises(TypeError):
-            r = Rectangle(5, 10, "1", 2, 3)
+            r = Square(5, 10, "1", 2)
 
     def test_negativeWidth(self):
         with self.assertRaises(ValueError):
-            print(Rectangle(-5, 10, 1, 2, 3).width)
+            print(Square(-5, 10, 1, 3).size)
