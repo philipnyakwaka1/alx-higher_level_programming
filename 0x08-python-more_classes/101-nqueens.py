4951f6a499ead5e0c8ae187e6c6d7e5e4d90037d
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-09-22 17:26:04 +0300

    queens

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import sys
+
+
+def is_safe(board, row, col, n):
+    for i in range(col):
+        if board[i] == row or \
+           board[i] - i == row - col or \
+           board[i] + i == row + col:
+            return False
+    return True
+
+
+def solve_nqueens(n):
+    def solve(board, col, n):
+        if col == n:
+            yield list(board)
+            return
+        for row in range(n):
+            if is_safe(board, row, col, n):
+                board[col] = row
+                yield from solve(board, col + 1, n)
+                board[col] = -1
+
+    board = [-1] * n  # Initialize the board with -1
+    solutions = list(solve(board, 0, n))
+    return solutions
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except Exception as ex:
+        sys.exit(1)
+
+    if n < 4:
+        sys.exit(1)
+
+    solutions = solve_nqueens(n)
+
+    for solution in solutions:
+        print("[", end="")
+        for i, row in enumerate(solution):
+            print("[{}, {}]".format(i, row), end="")
+            if i != len(solution) - 1:
+                print(", ", end="")
+        print("]")
