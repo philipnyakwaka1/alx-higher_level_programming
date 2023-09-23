b32da2a36427c4d531667be8418529222e4226b3
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-09-23 22:48:24 +0300

    created append file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""module for append_write"""
+"""Defines a file-appending function."""
 
 
-def write_file(filename="", text=""):
-    """appends a string to a text file (UTF8)
-    and returns the number of characters written
+def append_write(filename="", text=""):
+    """Appends a string to the end of a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
     """
-
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
