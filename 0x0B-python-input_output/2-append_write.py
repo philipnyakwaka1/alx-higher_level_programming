72d0867e99e812151e66218466324eb9e2294c6e
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-09-23 22:39:46 +0300

    created append file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module for append_write"""
+
+
+def write_file(filename="", text=""):
+    """appends a string to a text file (UTF8)
+    and returns the number of characters written
+    """
+
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
