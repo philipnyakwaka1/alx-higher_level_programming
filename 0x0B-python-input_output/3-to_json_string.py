dc7d77ba2c2c0f0938962bafaf8d868612d96a3b
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-09-27 13:12:43 +0300

    pycodestyle

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -3,8 +3,9 @@
 import json
 
 
-def from_json_string(my_str):
+def to_json_string(my_str):
     """returns an object (Python data structure)
     represented by a JSON string"""
 
     return json.dumps(my_str)
+
