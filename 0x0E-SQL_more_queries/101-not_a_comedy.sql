27c4c95eb9eff79d151d77a941be906a3fd24624
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-11-03 15:41:26 +0300

    modify

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,1 +1,7 @@
--- lists all shows from hbtn_0d_tvshows_rate by their rating.
+--  lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+SELECT tv_shows.title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name != 'Comedy' OR tv_genres.name IS NULL
+ORDER BY tv_shows.title ASC;
