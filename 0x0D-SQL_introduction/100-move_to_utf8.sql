0931dd7493b0727ec9b19aed06b6fda701f74b3d
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-11-03 12:28:55 +0300

    modify

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,10 +3,8 @@ ALTER DATABASE hbtn_0c_0
   CHARACTER SET utf8mb4
   COLLATE utf8mb4_unicode_ci;
 
--- Change the table's character set and collation
+use hbtn_0c_0;
+
 ALTER TABLE first_table
   CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Change the character set and collation of the name field
-ALTER TABLE first_table
-  MODIFY name varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
