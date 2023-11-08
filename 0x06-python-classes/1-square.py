f1f986a03a938fd8a0c6a6426abb191159ee4f01
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-11-08 20:31:51 +0300

    initializes square with private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Module for Square class"""
+
+
+class Square():
+    """Defines class square"""
+
+    def __init__(self, size):
+        """Initializes square of with size attribute"""
+        self.__size = size
