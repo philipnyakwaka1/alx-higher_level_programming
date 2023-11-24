5ef7cb247dbe3962f572895b7b6eec055d06b68e
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-11-24 22:47:15 +0300

    manage port

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,12 +6,14 @@ all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-def main():
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
-    cursor.execute("SELECT states.id, states.name FROM states ORDER BY states.id ASC;")
+    cursor.execute(
+        "SELECT states.id, states.name FROM states ORDER BY states.id ASC;")
     result = cursor.fetchall()
     for i in result:
         print(i)
-if __name__ == '__main__':
-    main()
+    cursor.close()
+    db.close()
