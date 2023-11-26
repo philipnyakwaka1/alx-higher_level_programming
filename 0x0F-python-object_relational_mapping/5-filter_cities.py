de8bc6c66332545da0ed4c782c27ffaf0ac3197e
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-11-26 19:23:43 +0300

    list cities in a state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,9 +16,11 @@ if __name__ == '__main__':
             = states.id WHERE states.name LIKE %s\
             ORDER BY cities.id ASC;", ('%' + input_value + '%', ))
     results = cursor.fetchall()
-    n = 1
-    for i in results:
-        my_list = [i[0] for i in results]
-    print(f'{", ".join(my_list)}')
+    if len(results) == 0:
+        print('\n')
+    else:
+        for i in results:
+            my_list = [i[0] for i in results]
+        print(f'{", ".join(my_list)}')
     cursor.close()
     db.close()
