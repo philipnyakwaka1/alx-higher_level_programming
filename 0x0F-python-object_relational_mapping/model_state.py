0014a8fa0d7c83f657cbf2d623328a24812b3ea9
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-11-28 11:29:12 +0300

    model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-This module contains the class definition
-of a State and an instance Base = declarative_base()
-"""
+"""Module for class state, an instance of declarative_base"""
 
 import MySQLdb
 from sqlalchemy import create_engine, Integer, String, Column
@@ -13,10 +10,10 @@ engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}\
                     @localhost/{sys.argv[3]}', pool_pre_ping=True)
 Base = declarative_base()
 
+
 class State(Base):
     """This class links to MySQL table states"""
 
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
