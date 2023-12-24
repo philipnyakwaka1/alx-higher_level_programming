aa5acdfca437951eed011eb1701ebf73d9117761
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-12-24 20:43:36 +0300

    display output size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a request to that URL, and displays the size of the body of the response in bytes
-curl -sI "$1" | grep -i Content-Length | awk {print $2}
+curl -sI "$1" | grep -i Content-Length | awk  '{print $2}'
