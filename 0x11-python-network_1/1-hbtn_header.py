d63facf96c908e6e99304fee16862d80ce36d46a
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-12-28 00:34:55 +0300

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,5 @@
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
 from urllib import request
 import sys
-url = sys.argv[1]
-with request.urlopen(url) as response:
+with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     print(response.headers)
