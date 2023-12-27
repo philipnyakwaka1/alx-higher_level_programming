b3419807f333a18d7b0a2ed55a21a84828901184
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-12-28 00:13:08 +0300

    1-hbtn_header.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,4 @@
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
 from urllib import request
 with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    content = response.read()
-    utf_content = content.decode('utf-8')
-    print('Body response:')
-    print(f'\t- type: {type(content)}')
-    print(f'\t- content: {content}')
-    print(f'\t- utf8 content: {utf_content}')
+    print(response.headers)
