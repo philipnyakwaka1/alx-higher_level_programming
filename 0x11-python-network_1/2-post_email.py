f567b8031d4947c6f51ad5fdde88dbfdd667254d
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-12-28 00:54:40 +0300

    1-hbtn_header.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email, sends a
+POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+
+from urllib import request
+import urllib
+import sys
+
+if __name__ == '__main__':
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values).encode('ascii')
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
+        print(response.read.decode('utf-8'))
