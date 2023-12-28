1a3cf0cf1e998386702d7fad0edba52fb9345e77
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-12-28 09:11:12 +0300

    reponse header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and displays the value
+of the variable X-Request-Id in the response header
+"""
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    response = requests.get(sys.argv[1])
+    if response.headers['X-Request-Id'] is not None:
+        print(response.headers['X-Request-Id'])
