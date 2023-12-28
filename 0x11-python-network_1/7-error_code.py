37ea694d6865c1ed586b68226e132240bc2d63c4
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-12-28 09:40:08 +0300

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,7 +8,7 @@ import sys
 import requests
 
 if __name__ == '__main__':
-    response = requests.post(sys.argv[1])
+    response = requests.get(sys.argv[1])
     if response.status_code >= 400:
         print(f'Error code: {response.status_code}')
     else:
