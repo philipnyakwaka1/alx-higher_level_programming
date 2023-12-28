1df2e06f0b5050b0e38deacb0248c12d82dcb0a2
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-12-28 09:06:22 +0300

    read response

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+import requests
+
+response = requests.get('https://alx-intranet.hbtn.io/status')
+print('Body response:')
+print(f'\t- type: {type(response.text)}')
+print(f'\t- content: {response.text}')
