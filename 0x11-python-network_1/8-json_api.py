519e9e7b165664cc5f38d5702901d5fe1147de21
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-12-28 09:54:45 +0300

    search api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    l = sys.argv[1] if len(sys.argv) > 1 else ""
+    value = {'q': l}
+    response = requests.post('http://0.0.0.0:5000/search_user', data=value)
+    try:
+        if len(response.json()) == 0:
+            print('No result')
+        else:
+            print(f'[{response.get("id")}] {response.get("name")}')
+    except Exception as e:
+        print('Not a valid JSON')
