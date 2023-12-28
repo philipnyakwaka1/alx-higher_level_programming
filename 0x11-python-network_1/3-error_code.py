d2fa344bc27eda5d5b8eeec2e1046d5d33f058d6
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-12-28 08:58:35 +0300

    Error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,7 +12,5 @@ if __name__ == '__main__':
     try:
         with request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
-    except URLError as e:
-        print(f'Reason: {e.reason}')
     except HTTPError as e:
         print(f'Error code: {e.code}')
