e5688c73894e0d658ac1bf8fc48e52aa87a4ab00
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-12-30 14:13:46 +0300

    delete match

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script that adds the State object “Louisiana”
+to the database hbtn_0e_6_usa
+"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from urllib.parse import quote
+import sys
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(quote(sys.argv[1]), quote(sys.argv[2]),
+                                   quote(sys.argv[3])), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    qry = session.query(State).filter(State.name.like('%a%')).all()
+    print(qry)
+    if len(qry) > 0:
+        for state in qry:
+            session.delete(state)
+            session.commit()
+    session.close()
