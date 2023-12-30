2b470c5203ac7023c8e4df9c1e0c52fa6e99fbcb
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-12-30 12:28:13 +0300

    mysql

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,12 +10,13 @@ from sqlalchemy import create_engine
 from urllib.parse import quote
 import sys
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(quote(sys.argv[1]), quote(sys.argv[2]),
-                               quote(sys.argv[3])), pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
-session = Session()
-query_object = session.query(State).first()
-if query_object is not None:
-    print(f'{query_object.id}: {query_object.name}')
-session.close()
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(quote(sys.argv[1]), quote(sys.argv[2]),
+                                   quote(sys.argv[3])), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query_object = session.query(State).first()
+    if query_object is not None:
+        print(f'{query_object.id}: {query_object.name}')
+    session.close()
