ab014110f40cba224dd1addb8348db88be3bf834
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-10 17:38:37 +0300

    my array

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
-const myArray = [];
-const argv = process.argv.splice(2);
-for (let i = 0; i < argv.length; i++) {
-  myArray.push(parseInt(argv[i]));
+const vector = process.argv;
+if (vector.length === 2 || vector.length == 3) {
+  console.log(0);
+} else {
+  const myArray = [];
+  const argv = vector.splice(2);
+  for (let i = 0; i < argv.length; i++) {
+    myArray.push(parseInt(argv[i]));
+  }
+  console.log(myArray.sort((a, b) => b - a)[1]);
 }
-console.log(myArray.sort((a, b) => b - a)[1]);
