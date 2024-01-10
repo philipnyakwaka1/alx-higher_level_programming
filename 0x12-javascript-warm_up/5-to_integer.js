46629c3cdd86208e5bff0f90761e5919c59988b3
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-10 10:59:01 +0300

    parseInt

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const number = parseInt(process.argv[2]);
 if (isNaN(number)) {
-  console.log('Not a Number');
+  console.error('Not a Number');
 } else {
   console.log('My number:', number);
 }
