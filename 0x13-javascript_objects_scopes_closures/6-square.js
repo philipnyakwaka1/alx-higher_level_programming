d8650c963b693bfce4c17930629f2cbea37ac353
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-11 07:19:27 +0300

    print square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const parentSquare = require('./5-square');
+class Square extends parentSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
