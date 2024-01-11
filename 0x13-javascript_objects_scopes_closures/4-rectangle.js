4b022ee5c64d600368e2ad38e2b4c41c16fe2aa4
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-11 07:03:55 +0300

    rectangle double

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,23 +1,25 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w !== undefined && w > 0 && h !== undefined && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
-  rotate() {
+
+  rotate () {
     const tmp = this.width;
     this.width = this.height;
     this.height = tmp;
   }
-  double() {
+
+  double () {
     this.width = this.width * 2;
     this.height = this.height * 2;
   }
