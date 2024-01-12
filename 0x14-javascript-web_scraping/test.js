eb44415afed1d0f959f12af6a135e7343a3af484
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-12 05:43:20 +0300

    writeFile

--- 0x14-javascript-web_scraping/test.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-fs.readFilePromise(process.argv[2], 'utf-8')
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
