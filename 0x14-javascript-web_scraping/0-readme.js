0c8a5db1bc79bf16443fc1b3db29a5fc24853404
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-12 05:48:15 +0300

    writeFile

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,3 +6,4 @@ fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   }
   console.log(data);
 });
+console.log('type of fs is' + typeof fs);
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,4 +4,5 @@ fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   }
+  console.log(process.argv[3]);
 });
