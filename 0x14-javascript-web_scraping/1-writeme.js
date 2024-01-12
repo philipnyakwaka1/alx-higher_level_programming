5ce2a595d2abdab3325b7900f052698662a99d5b
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-12 05:55:47 +0300

    writeFile

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err, data) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
   if (err) {
     console.log(err);
   }
-  console.log(process.argv[3]);
 });
