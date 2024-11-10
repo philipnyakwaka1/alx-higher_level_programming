691f1d32636fed46e7943b9674eea611742e3022
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-11-10 21:12:33 +0300

    chore: Add script to convert hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,6 @@
--- convert hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
--- SHOW VARIABLES LIKE 'character_set%';
--- SHOW VARIABLES LIKE 'collation%';
+-- Change collation in database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE hbtn_0c_0;
+
+use hbtn_0c_0;
+
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
