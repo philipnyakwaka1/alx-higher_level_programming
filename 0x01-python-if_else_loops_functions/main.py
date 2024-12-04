00658aac63a0c8239a452642d222fe4a2e1f223b
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-12-04 16:47:26 +0300

    two digit comb

--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
-for i in range(2, 10, 2):
-    print(i, end=" ")
+"""Print different two digit combinations"""
+for i in range(10):
+    for  j in range(i + 1, 10):
+        if j == 9 and i != 8:
+            print('{}{}'.format(i, j),end=", ")
+        else:
+            if j == 9 and i == 8:
+                print('{}{}'.format(i, j))
+            elif j != i:
+                print('{}{}'.format(i, j),end=", ")
